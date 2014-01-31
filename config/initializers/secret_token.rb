7e98166c6c3c794444a41d029f66e7cc781276fa
Jean Nguyen <sentrion@gmail.com>
2014-01-31 07:16:49 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Anarchy::Application.config.secret_key_base = '59ec85ab7936a6b2199a10ea1079802b0aa21ed21fd0698a62b2467d4aa36702a3aa2302637b51111880662625c3011f2ebbcafec3bed7e39d90d35364d48a98'
